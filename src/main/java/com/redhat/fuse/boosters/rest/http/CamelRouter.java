eadf5594a6201b2f4b9f8766502132e1aca6965c
tgubeli <jtomasg@gmail.com>
2021-06-11 00:28:42 -0400

    commit inicial

---
+++ src/main/java/com/redhat/fuse/boosters/rest/http/CamelRouter.java
@@ -0,0 +1,39 @@
+package com.redhat.fuse.boosters.rest.http;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.stereotype.Component;
+
+/**
+ * A simple Camel REST DSL route that implements the greetings service.
+ * 
+ */
+@Component
+public class CamelRouter extends RouteBuilder {
+
+    @Override
+    public void configure() throws Exception {
+
+        // @formatter:off
+        restConfiguration()
+                .apiContextPath("/api-doc")
+                .apiProperty("api.title", "Save File REST API")
+                .apiProperty("api.version", "1.0")
+                .apiProperty("cors", "true")
+                .apiProperty("base.path", "camel/")
+                .apiProperty("api.path", "/")
+                .apiProperty("host", "")
+                .apiContextRouteId("doc-api")
+            .component("servlet")
+            .bindingMode(RestBindingMode.json);
+        
+        rest("/save/file").description("Save File in Filesystem")
+            .get("/pdf").route().routeId("save-file-api")
+                .to("direct:save-file");
+
+        from("direct:save-file").description("Save File as PDF")
+            .streamCaching()
+            .to("file://files/out/pdf");     
+    }
+
+}
