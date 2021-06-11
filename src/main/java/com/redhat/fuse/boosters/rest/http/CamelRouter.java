5203232bc9ee6ca274a522fb77022a734ccf3476
tgubeli <jtomasg@gmail.com>
2021-06-11 10:37:05 -0400

    add content type to rest endpoint consume and producer

--- src/main/java/com/redhat/fuse/boosters/rest/http/CamelRouter.java
+++ src/main/java/com/redhat/fuse/boosters/rest/http/CamelRouter.java
@@ -3,6 +3,7 @@ package com.redhat.fuse.boosters.rest.http;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.model.rest.RestBindingMode;
 import org.springframework.stereotype.Component;
+import org.apache.camel.Exchange;
 
 /**
  * A simple Camel REST DSL route that implements the greetings service.
@@ -28,13 +29,16 @@ public class CamelRouter extends RouteBuilder {
             .bindingMode(RestBindingMode.json);
         
         rest("/save/file").description("Save File in Filesystem")
-            .get("/pdf").consumes("text/plain").produces("text/html")
+            .get("/pdf").consumes("text/plain").produces("application/json")
             .route().routeId("save-file-api")
             .to("direct:save-file");
 
         from("direct:save-file").description("Save File as PDF")
             .streamCaching()
-            .to("file://files/out/pdf?fileName=sampleFile.pdf");
+            .setHeader(Exchange.CONTENT_TYPE, constant("application/pdf"))
+            .to("file://files/out/pdf?fileName=sampleFile.pdf")
+            .setHeader(Exchange.CONTENT_TYPE, constant("application/json"))
+            .setBody(constant("{'status':'File transformed ok'}"));
 
     }
 
